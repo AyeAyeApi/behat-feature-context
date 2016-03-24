e8f2e83b32a7acb757bfa1fd16248ee44dfaccbc
Daniel Mason <daniel@danielmason.com>
2016-03-24 13:53:39 +0000

    Added some functionality to the mock for testing

--- mock/Api/Version1.php
+++ mock/Api/Version1.php
@@ -10,8 +10,38 @@
 namespace AyeAye\Behat\Mock\Api;
 
 use AyeAye\Api\Controller;
+use AyeAye\Api\Exception as AyeAyeException;
 
 class Version1 extends Controller
 {
+
+    /**
+     * Version1 constructor.
+     * Removes the coffee endpoint from the index.
+     */
+    public function __construct()
+    {
+        $this->hideMethod('getCoffeeEndpoint');
+    }
+
+    /**
+     * Self referenced controller
+     * @return $this
+     * @throws AyeAyeException
+     */
+    public function version1Controller()
+    {
+        $this->hideMethod('version1Controller');
+        return $this;
+    }
+
+    /**
+     * I am a teapot
+     * @throws AyeAyeException
+     */
+    public function getCoffeeEndpoint()
+    {
+        throw new AyeAyeException(418);
+    }
 
 }
